48d2558c8a37f63d8c01855a714db26fa8b1ed4b
DanKos22 <G00397054@atu.ie>
2024-11-07 22:29:53 +0200

    new commit

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 app.use(cors());
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
     res.send('Hello from the Node.js backend!');
+});
+
+app.post('/api/sendMessage', (req, res) => {
+    const message = req.body; // Retrieve JSON message from the request body
+    console.log("Successful connection to ESP32", message);
 });
 
 app.listen(port, () => {
